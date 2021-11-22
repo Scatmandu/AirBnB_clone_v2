8eed770b549793e399e7d968eed2fee0f57a379b
lyndonpett <lyndonpett98@gmail.com>
2021-11-22 14:28:37 -0600

    removing if check

--- models/state.py
+++ models/state.py
@@ -12,15 +12,16 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
 
-    @property
-    def cities(self):
-        """getter for cities"""
-        from models import storage
-        get_cities = []
-        for k, v in storage.all(City).items():
-            if v.state_id == self.id:
-                get_cities.append(v)
-        return get_cities
+    else:
+        @property
+        def cities(self):
+            """"""
+            list_cities = []
+            for item in models.storage.all(City).values():
+                if item.state_id == self.id:
+                    list_cities.append(item)
+            return list_cities
