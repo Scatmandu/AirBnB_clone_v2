cbea5a2b3b0f9487add5f6f356813045c4808a06
lyndonpett <lyndonpett98@gmail.com>
2021-11-23 10:17:16 -0600

    Testing 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    '''defines class Amenity'''
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        'Place', secondary='place_amenity', viewonly=False)
