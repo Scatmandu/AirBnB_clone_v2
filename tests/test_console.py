36d3366a5e299da98ae1b05bcf6c74c8857b502f
Scatmandu <mcclur05@nsuok.edu>
2021-11-23 22:10:46 -0600

    added first test

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,3 +12,9 @@ import os
 
 class TestConsole(unittest.TestCase):
     """tests for the console"""
+
+    def test_all(self):
+    """ test all """
+    with patch('sys.stdout', new=StringIO()) as file:
+        HBNBCommand().onecmd("all asdf")
+        self.assertEqual(file.getvalue(), "\n** class doesn't exist **\n")
