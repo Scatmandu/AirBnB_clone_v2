4b85406f072624de4b9ff06158c0c06643b28d6f
Scatmandu <mcclur05@nsuok.edu>
2021-11-24 18:05:02 -0600

    added create test

---
+++ tests/test_console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""console Unittests"""
+
+
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+import unittest
+import sys
+
+
+class TestConsole(unittest.TestCase):
+    """tests for the console"""
+
+    def test_create(self):
+    """ test all """
+    with patch('sys.stdout', new=StringIO()) as file:
+        HBNBCommand().onecmd("create widdly")
+        self.assertEqual(file.getvalue(), "\n** class doesn't exist **\n")
+
+    def test_all(self):
+    """ test all """
+    with patch('sys.stdout', new=StringIO()) as file:
+        HBNBCommand().onecmd("all widdly")
+        self.assertEqual(file.getvalue(), "\n** class doesn't exist **\n")
+
+        create show destroy all update
