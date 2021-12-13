3056de178a616a2f4799eb64e7f8f8da96bc4dfe
Scatmandu <mcclur05@nsuok.edu>
2021-12-12 18:35:25 -0600

    typo ln24 col34

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,7 @@ def C(text):
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', stricct_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
